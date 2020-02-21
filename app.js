a426c01d1ac35a7ae5c8eea351509d99fbdda8d9
ogbeche77 <ogbeche77@yahoo.com>
2020-02-21 20:54:27 +0100

    register and login handle

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
 const express = require("express");
 const expressLayouts = require("express-ejs-layouts"); //initialize ejs in app.js
+const mongoose = require("mongoose"); //we introduce mongoose
+
 const app = express();
 
+// Database config
+const db = require("./config/keys").MongoURI;
+
+// Connect to Mongo
+mongoose.connect (db, { useNewUrlParser: true})
+.then(() => console.log("MongoDB connected"))
+.catch(err => console.log(err));
+
+
+//EJS
 //add middleware
 app.use(expressLayouts);
 //set view engine to ejs
 app.set ("view engine", "ejs");
 
-
+//Bodyparser
+app.use(express.urlencoded({ extended: false}));
 
 
 //Routes
--- routes/users.js
+++ routes/users.js
@@ -2,10 +2,17 @@ const express = require("express");
 const router = express.Router();
 
 //SignIn Page
-router.get("/login", (req, res)=> res.send("Login"));
+router.get("/login", (req, res)=> res.render("Login"));
 
 
 //Register Page
-router.get("/register", (req, res)=> res.send("register"));
+router.get("/register", (req, res)=> res.render("register"));
+
+// Register Handle
+router.post("/register",(req, res)=>{
+    console.log(req.body)
+    res.send("hello eveyone");
+});
+
 
 module.exports = router;

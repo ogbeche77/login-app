3913bb9f06e795a3d1c4297094bd00e43f52314b
ogbeche77 <ogbeche77@yahoo.com>
2020-02-22 21:17:17 +0100

    flash msg added

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const expressLayouts = require("express-ejs-layouts"); //initialize ejs in app.js
 const mongoose = require("mongoose"); //we introduce mongoose
+const flash = require("connect-flash");
+const session = require("express-session");
 
 const app = express();
 
@@ -21,6 +23,23 @@ app.set ("view engine", "ejs");
 
 //Bodyparser
 app.use(express.urlencoded({ extended: false}));
+
+// Express Session middleware
+app.use(session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true,
+  }));
+
+  //connect flash middleware, to display msg in login page
+  app.use(flash());
+
+  //Global vars
+  app.use((req, res, next)=>{
+      res.locals.success_msg = req.flash("success_msg");
+      res.locals.error_msg = req.flash("error_msg");
+      next();
+  });
 
 
 //Routes

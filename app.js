1c6ea51b5f0c9cedb0c1f2338f5eee8b6ac50f96
ogbeche77 <ogbeche77@yahoo.com>
2020-04-08 12:30:12 +0200

    formatted

--- app.js
+++ app.js
@@ -3,37 +3,29 @@ const expressLayouts = require("express-ejs-layouts"); //initialize ejs in app.j
 const mongoose = require("mongoose"); //we introduce mongoose
 const flash = require("connect-flash");
 const session = require("express-session");
-const passport = require ("passport");
-
+const passport = require("passport");
 
 const app = express();
 
 // Passport config
-require("./config/passport") (passport);
-
-
-
+require("./config/passport")(passport);
 
 // Database config
 const db = require("./config/keys").MongoURI;
 
 // Connect to Mongo
-mongoose.connect (db, { useNewUrlParser: true})
-.then(() => console.log("MongoDB connected"))
-.catch(err => console.log(err));
-
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log(err));
 
 //EJS
 //add middleware
 app.use(expressLayouts);
 
-
 //set view engine to ejs
 
-app.set ("view engine", "ejs");
-
-
-
+app.set("view engine", "ejs");
 
 /*
 app.set('views', path.join(__dirname, 'views'));
@@ -42,36 +34,36 @@ app.set('view engine', 'html');
 */
 
 //Bodyparser
-app.use(express.urlencoded({ extended: false}));
+app.use(express.urlencoded({ extended: false }));
 
 // Express Session middleware
-app.use(session({
-    secret: 'secret',
+app.use(
+  session({
+    secret: "secret",
     resave: true,
     saveUninitialized: true,
-  }));
+  })
+);
 
-  //Passport middleware
+//Passport middleware
 app.use(passport.initialize());
 app.use(passport.session());
 
-  //connect flash middleware, to display msg in login page
-  app.use(flash());
+//connect flash middleware, to display msg in login page
+app.use(flash());
 
-  //Global vars
-  app.use((req, res, next)=>{
-      res.locals.success_msg = req.flash("success_msg"); // for registration flash
-      res.locals.error_msg = req.flash("error_msg");  // for registration flash
-      res.locals.error = req.flash("error"); //for login flash
-      next();
-  });
-
+//Global vars
+app.use((req, res, next) => {
+  res.locals.success_msg = req.flash("success_msg"); // for registration flash
+  res.locals.error_msg = req.flash("error_msg"); // for registration flash
+  res.locals.error = req.flash("error"); //for login flash
+  next();
+});
 
 //Routes
 app.use("/", require("./routes/index"));
 app.use("/users", require("./routes/users"));
 
 const PORT = process.env.PORT || 5000;
-
 
 app.listen(PORT, console.log(`server started on port ${PORT}`));
